68b42f9e5f2c527c518dea0550787da0888bf38b
Ismael Celis <ismaelct@gmail.com>
2011-05-30 17:50:38 +0100

    Scope tests: put common object in setup block

--- test/scope_tests.js
+++ test/scope_tests.js
@@ -1,86 +1,56 @@
-var User = Catwalk.Model('user').include({
-  name: function () {
-    return this.attr('name')
-  },
-  online: function () {
-    return this.attr('online');
+module('Scopes', {
+  setup: function () {
+    this.User = Catwalk.Model('user').include({
+      name: function () {
+        return this.attr('name')
+      },
+      online: function () {
+        return this.attr('online');
+      }
+    });
+    
+    this.scope = new Catwalk.Scope(this.User);
   }
 });
 
-// user_list.bind('add', function (m) {
-//   results.push('Model added '+m.name())
-// })
-// 
-// user_list.scope('johns', function (model) {
-//   return /^joh/i.test(model.name());
-// });
-// 
-// user_list.online.bind('add', function (m) {
-//   results.push('User online '+m.name())
-// }).bind('remove', function (m) {
-//   results.push('Online user removed '+m.name())
-// });
-// 
-// user_list.offline.bind('add', function (m) {
-//   results.push('User offline '+m.name())
-// }).bind('remove', function (m) {
-//   results.push('Offline user removed '+m.name())
-// })
-// 
-// // Bind to scope chains
-// var chained_results = [];
-// var chained_scope = user_list.scope('online').scope('johns').bind('add', function (m) {
-//   chained_results.push('Chained model added '+m.name())
-// }).bind('remove', function (m) {
-//   chained_results.push('Chained model removed '+m.name())
-// })
-
-
-
-module('Scopes');
-
 test('it should save in subscribed collections', function () {
-  var scope = new Catwalk.Scope(User);
-  var user = new User({name: 'ismael', online: true});
-  equal(user, scope.last())
-  equal(1, scope.size())
+  var user = new this.User({name: 'ismael', online: true});
+  equal(user, this.scope.last())
+  equal(1, this.scope.size())
 });
 
 test('it should save in relevant scopes', function () {
-  var scope = new Catwalk.Scope(User);
-  var user = new User({name: 'ismael', online: true});
+  var user = new this.User({name: 'ismael', online: true});
   
-  scope.scope('online', function (model) {
+  this.scope.scope('online', function (model) {
     return model.online();
   });
 
-  scope.scope('offline', function (model) {
+  this.scope.scope('offline', function (model) {
     return !model.online();
   });
   
-  var user = new User({name: 'ismael', online: true});
+  var user = new this.User({name: 'ismael', online: true});
   
-  equal(user, scope.online.last())
-  equal(1, scope.online.size(), 'should add to one')
-  equal(0, scope.offline.size(), 'should remove from another')
+  equal(user, this.scope.online.last())
+  equal(1, this.scope.online.size(), 'should add to one')
+  equal(0, this.scope.offline.size(), 'should remove from another')
 })
 
 test('it should run attached handlers', function () {
   
-  var scope = new Catwalk.Scope(User);
-  
-  scope.scope('online', function (model) {
+  this.scope.scope('online', function (model) {
     return model.online();
   });
   
   var results = [];
-  scope.bind('add', function (m) {
+  this.scope.bind('add', function (m) {
     results.push('Model added '+m.name())
   })
-  scope.online.bind('add', function (m) {
+  this.scope.online.bind('add', function (m) {
     results.push('User online '+m.name())
   })
-  var user = new User({name: 'ismael', online: true});
+  var user = new this.User({name: 'ismael', online: true});
   
   equal(results[0], 'Model added ismael')
   equal(results[1], 'User online ismael')
@@ -88,48 +58,45 @@ test('it should run attached handlers', function () {
 
 test('it should update all scopes when instance changes', function () {
   
-  var scope = new Catwalk.Scope(User);
-  
-  scope.scope('online', function (model) {
+  this.scope.scope('online', function (model) {
     return model.online();
   });
-  scope.scope('offline', function (model) {
+  this.scope.scope('offline', function (model) {
     return !model.online();
   });
   
   var results = [];
-  scope.bind('add', function (m) {
+  this.scope.bind('add', function (m) {
     results.push('Model added '+m.name())
   })
-  scope.online.bind('add', function (m) {
+  this.scope.online.bind('add', function (m) {
     results.push('User online '+m.name())
   })
-  var user = new User({name: 'ismael', online: true});
+  var user = new this.User({name: 'ismael', online: true});
   
   user.attr('online', false);
-  equal(0, scope.online.size());
-  equal(1, scope.offline.size());
-  equal(user, scope.offline.last())
+  equal(0, this.scope.online.size());
+  equal(1, this.scope.offline.size());
+  equal(user, this.scope.offline.last())
 })
 
 test('it should chain scopes', function () {
-  var scope = new Catwalk.Scope(User);
   
-  scope.scope('online', function (model) {
+  this.scope.scope('online', function (model) {
     return model.online();
   });
-  scope.scope('johns', function (model) {
+  this.scope.scope('johns', function (model) {
     return /^joh/i.test(model.name());
   });
   // Bind to scope chains
   var chained_results = [];
-  var chained_scope = scope.scope('online').scope('johns').bind('add', function (m) {
+  var chained_scope = this.scope.scope('online').scope('johns').bind('add', function (m) {
     chained_results.push('Chained model added '+m.name())
   }).bind('remove', function (m) {
     chained_results.push('Chained model removed '+m.name())
   })
   
-  var u = new User({name: 'John', online: true});
+  var u = new this.User({name: 'John', online: true});
   equal(1, chained_scope.size(), 'increments chained_scope count by 1');
   equal(u, chained_scope.last(), 'new model is in chained_scope collection');
   equal(chained_results[0], 'Chained model added John', 'appends to callback results');
@@ -140,7 +107,7 @@ test('it should chain scopes', function () {
   
   // test that scope works standalone too
   var standalone_results = [];
-  var standalone = scope.scope('johns').bind('add', function (model) {
+  var standalone = this.scope.scope('johns').bind('add', function (model) {
     standalone_results.push('Standalone john added')
   })
   u.attr({name:'John', online: false});
